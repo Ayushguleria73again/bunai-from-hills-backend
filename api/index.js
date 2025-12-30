51ba351ea2cf0c9ed51e56e4156a7986614c5c99
Ayush Guleria <ayush@Ayushs-MacBook-Air.local>
2025-12-30 19:58:35 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -5,6 +5,17 @@ const cors = require("cors")
 const path = require("path")
 
 const app = express()
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204)
+  }
+
+  next()
+})
 
 const PORT = process.env.PORT
 
