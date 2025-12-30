e27b87aa88fd00b87a2fea1b3b272f376d8dfd83
Ayush Guleria <ayush@Ayushs-MacBook-Air.local>
2025-12-30 18:36:58 +0530

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001
 // MIDDLEWARE
 // --------------------
 app.use(cors({
-  origin: "*",
+  origin: ["*", process.env.PORT1],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
